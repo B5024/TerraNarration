911f8a517bce6e0e1c78dedc488d43f100b84cb5
B5024 <2598882405@qq.com>
2025-12-24 16:10:29 +0800

    修复QuoteLibrary和quotes.json的缺失

--- Common/QuoteLibrary.cs
+++ Common/QuoteLibrary.cs
@@ -6,21 +6,30 @@ using Terraria.ModLoader;
 
 namespace TerraNarration.Common
 {
-    public class QuoteLibrary
-    {
-        public List<string> OnHitByNPCQuotes { get; set; }
-        public List<string> OnHitByProjectileQuotes { get; set; }
-        public List<string> LowHealthQuotes { get; set; }
-        public List<string> MiningQuotes { get; set; }
-        public List<string> AxingQuotes { get; set; }
+  public class QuoteLibrary
+  {
+    // 战斗相关
+    public List<string> OnHitByNPCQuotes { get; set; }
+    public List<string> OnHitByProjectileQuotes { get; set; }
+    public List<string> LowHealthQuotes { get; set; }
+    public List<string> LowManaQuotes { get; set; }
 
-        public static QuoteLibrary Load(Mod mod)
-        {
-            // 从 Mod 的资源中获取文件流
-            using Stream stream = mod.GetFileStream("quotes.json");
-            using StreamReader reader = new StreamReader(stream, Encoding.UTF8);
-            string json = reader.ReadToEnd();
-            return JsonConvert.DeserializeObject<QuoteLibrary>(json);
-        }
+    // 劳作相关
+    public List<string> MiningQuotes { get; set; }
+    public List<string> AxingQuotes { get; set; }
+
+    // Boss
+    public List<string> SkeletronSpawnQuotes { get; set; }
+    public List<string> SkeletronStarCannonQuotes { get; set; }
+    public List<string> EyeOfCthulhuSpawnQuotes { get; set; }
+
+    public static QuoteLibrary Load(Mod mod)
+    {
+      // 从 Mod 的资源中获取文件流
+      using Stream stream = mod.GetFileStream("quotes.json");
+      using StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+      string json = reader.ReadToEnd();
+      return JsonConvert.DeserializeObject<QuoteLibrary>(json);
     }
+  }
 }
